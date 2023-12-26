404220f1d1404896fb16ad9d535ce2cc61b817e8
Aravind <aravindbujji7@gmail.com>
2023-12-26 14:37:12 +0530

    Changed variable name as naming covention and formated the code

--- src/com/JDBC/DML/DMLOperations.java
+++ src/com/JDBC/DML/DMLOperations.java
@@ -5,36 +5,34 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-
-
 public class DMLOperations {
 	static Scanner scn = new Scanner(System.in);
 	static int id;
 	static String firstname;
 	static String lastname;
-	
+
 	public static void insertRecord(Statement statement) throws SQLException {
 		runtimeData();
-		String quaryString = "insert into employee values ("+id+",'"+firstname+"','"+lastname+"')";
+		String quaryString = "insert into employee values (" + id + ",'" + firstname + "','" + lastname + "')";
 		System.out.println(quaryString);
 		int rows = statement.executeUpdate(quaryString);
-		System.out.println(rows+" rows  got inserted");
+		System.out.println(rows + " rows  got inserted");
 	}
-	
+
 	public static void selectRecords(Statement statement) throws SQLException {
-		
+
 		String quaryString = "select * from employee";
 		System.out.println(quaryString);
 		ResultSet resultSet = statement.executeQuery(quaryString);
 		System.out.println("-----------------------------------------------");
-		while(resultSet.next()) {
-			System.out.println(resultSet.getString(1)+"\t"+resultSet.getString(2)+"\t"+resultSet.getString(3));
+		while (resultSet.next()) {
+			System.out.println(resultSet.getString(1) + "\t" + resultSet.getString(2) + "\t" + resultSet.getString(3));
 		}
 		System.out.println("------------------------------------------------");
 	}
-	
+
 	public static void updateRecord(Statement statement) throws SQLException {
-		
+
 		System.out.println("Please Enter ID to Update Record");
 		int id = scn.nextInt();
 		System.out.println("Select following options for Update the Record");
@@ -46,49 +44,50 @@ public class DMLOperations {
 		case 1: {
 			System.out.println("Enter FirstName");
 			String firstName = scn.next();
-			String quaryString = "update employee set firstname="+"'"+firstName+"'"+"where id ="+id;
+			String quaryString = "update employee set firstname=" + "'" + firstName + "'" + "where id =" + id;
 			System.out.println(quaryString);
 			int rows = statement.executeUpdate(quaryString);
-			System.out.println(rows+" row got updated");
+			System.out.println(rows + " row got updated");
 			break;
 		}
-		case 2:{
+		case 2: {
 			System.out.println("Enter LastName");
 			String lastName = scn.next();
-			String quaryString = "update employee set lastname="+"'"+lastName+"'"+"where id ="+id;
+			String quaryString = "update employee set lastname=" + "'" + lastName + "'" + "where id =" + id;
 			System.out.println(quaryString);
 			int rows = statement.executeUpdate(quaryString);
-			System.out.println(rows+" row got updated");
+			System.out.println(rows + " row got updated");
 			break;
 		}
-		case 3:{
+		case 3: {
 			System.out.println("Enter FirstName");
 			String firstName = scn.next();
 			System.out.println("Enter LastName");
 			String lastName = scn.next();
-			String quaryString = "update employee set firstname="+"'"+firstName+"',"+"lastname="+"'"+lastName+"'"+"where id ="+id;
+			String quaryString = "update employee set firstname=" + "'" + firstName + "'," + "lastname=" + "'"
+					+ lastName + "'" + "where id =" + id;
 			System.out.println(quaryString);
 			int rows = statement.executeUpdate(quaryString);
-			System.out.println(rows+" row got updated");
+			System.out.println(rows + " row got updated");
 			break;
 		}
 		default:
 			System.out.println("-----Invalid Entry------");
 		}
-		
+
 	}
-	
+
 	public static void deleteRecord(Statement statement) throws SQLException {
 		System.out.println("Enter Id to delete a record");
 		int id = scn.nextInt();
-		String quaryString = "delete from employee where id="+id;
+		String quaryString = "delete from employee where id=" + id;
 		int rows = statement.executeUpdate(quaryString);
-		System.out.println(rows+" got deleted");
+		System.out.println(rows + " got deleted");
 	}
 
 	private static void runtimeData() {
 		System.out.println("Please enter id");
-		id =scn.nextInt();
+		id = scn.nextInt();
 		System.out.println("Please enter first name");
 		firstname = scn.next();
 		System.out.println("Please enter second name");
--- src/com/JDBC/JDBC_Application.java
+++ src/com/JDBC/JDBC_Application.java
@@ -9,60 +9,63 @@ import java.util.Scanner;
 import com.JDBC.Constants.Constants;
 import com.JDBC.DML.DMLOperations;
 
-
 public class JDBC_Application {
-	
+
 	static Connection connection = null;
-	static Statement statement=null;
+	static Statement statement = null;
 	static Scanner scnScanner = new Scanner(System.in);
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws SQLException {
+
 		try {
 			connection = getConnetion();
 			statement = getStatement(connection);
-			int Count=0;
-			while(Count<5) {
-			System.out.println("Please Select from following Operations");
-			System.out.println("1.Insert");
-			System.out.println("2.Update");
-			System.out.println("3.Delete");
-			System.out.println("4.Select");
-			int input = scnScanner.nextInt();
-			switch (input) {
-			case 1: {
-				DMLOperations.insertRecord(statement);
-				break;
+			int count = 0;
+			while (count < 5) {
+				System.out.println("Please Select from following Operations");
+				System.out.println("1.Insert");
+				System.out.println("2.Update");
+				System.out.println("3.Delete");
+				System.out.println("4.Select");
+				int input = scnScanner.nextInt();
+				switch (input) {
+				case 1: {
+					DMLOperations.insertRecord(statement);
+					break;
+				}
+				case 2: {
+					DMLOperations.updateRecord(statement);
+					break;
+				}
+				case 3: {
+					DMLOperations.deleteRecord(statement);
+					break;
+				}
+				case 4: {
+					DMLOperations.selectRecords(statement);
+					break;
+				}
+				default:
+					System.out.println("Invalid Selection..!");
+					count++;
+				}
 			}
-			case 2: {
-				DMLOperations.updateRecord(statement);
-				break;
-			}
-			case 3: {
-				DMLOperations.deleteRecord(statement);
-				break;
-			}
-			case 4: {
-				DMLOperations.selectRecords(statement);
-				break;
-			}
-			default:
-				System.out.println("Invalid Selection..!");
-				Count++;
-			}
-		}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			connection.close();
+			statement.close();
 		}
 
 	}
 
 	private static Connection getConnetion() throws SQLException {
-		Connection connection = DriverManager.getConnection(Constants.DBURL,Constants.DBUSER,Constants.DBPASSWORD);
+		Connection connection = DriverManager.getConnection(Constants.DBURL, Constants.DBUSER, Constants.DBPASSWORD);
 		System.out.println("Connection Established with DB");
 		return connection;
 	}
-	
+
 	private static Statement getStatement(Connection con) throws SQLException {
 		statement = con.createStatement();
 		return statement;
